113822f2a20134a478a1a6ebc614335b0b30b942
jvcataquiz <jvcataquiz@gmail.com>
2022-09-07 08:55:15 +0800

    additional page weather app with 2 api's fist api is rest for all countries next is openweather app

---
+++ js/allcountry.js
@@ -0,0 +1,47 @@
+ 
+document.addEventListener('DOMContentLoaded', () => {
+ 
+    const selectDrop = document.querySelector('#allcountries');
+    // const selectDrop = document.getElementById('countries');
+    fetch('https://restcountries.com/v3.1/all').then(res => {
+    return res.json();
+    }).then(data => {
+    let output = "";
+    data.forEach(country => {
+
+        const { name } = country
+
+      output += `<option value="${name.common}"> ${name.common}  </option>`;
+    })
+     
+    selectDrop.innerHTML = output;
+     
+    }).catch(err => {
+    console.log(err);
+    })
+     
+    });
+
+
+    const button = document.getElementById('buttons');
+    button.addEventListener('click', () => {
+        const selectedvalue = document.getElementById('allcountries');
+
+        var value = selectedvalue.options[selectedvalue.selectedIndex].text;
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='+ value+'&appid=6b23c082a2ca2bcdc9fff6eed8a38279')
+        .then(res => {
+            return res.json();
+            }).then(datadisplay
+            ).catch(err => {
+            console.log(err);
+            })
+             
+            });
+    
+
+            const datadisplay=(weather)=> {
+                document.getElementById('MyFrame').innerHTML  = `${weather.main.temp}`;
+ 
+            }
+        
+     
