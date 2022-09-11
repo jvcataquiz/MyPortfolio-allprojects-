398b934f75b7b2adf487b83374f0da12643a8326
jvcataquiz <jvcataquiz@gmail.com>
2022-09-11 14:10:33 +0800

    added dropdown for the Projects, if the user want specific programming language

--- js/project.js
+++ js/project.js
@@ -1,50 +1,56 @@
+
+ var seletedproject ="";
+ 
+function myFunction() {
+  var selectedvalue = document.getElementById("selected");
+ 
+   seletedproject =  selectedvalue.options[selectedvalue.selectedIndex].value;
 const car = {
       "data": [
-        {
+        { 
+          "projectlanguage": "C#",
           "imagelink": 'https://www.pngfind.com/pngs/m/683-6834213_transparent-html-logo-png-png-download.png',
-          "title": "Greeny Earth",
+          "title": "1",
           "description":" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasratione vel inventore labore commodi modi quos culpa aut saepe!Alias!",
           "projectlink":"#"
         },
-        {
-          "imagelink": "https://www.pngfind.com/pngs/m/683-6834213_transparent-html-logo-png-png-download.png",
-          "title": "C# prject",
-          "description":" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasratione vel inventore labore commodi modi quos culpa aut saepe!Alias!",
-          "projectlink":"#"
-        }
-        ,
-        {
-          "imagelink": "https://www.pngfind.com/pngs/m/683-6834213_transparent-html-logo-png-png-download.png",
-          "title": "C# prject",
+        { 
+          "projectlanguage": "java",
+          "imagelink": 'https://www.pngfind.com/pngs/m/683-6834213_transparent-html-logo-png-png-download.png',
+          "title": "java",
           "description":" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasratione vel inventore labore commodi modi quos culpa aut saepe!Alias!",
           "projectlink":"#"
         },
-        {
-          "imagelink": "https://www.pngfind.com/pngs/m/683-6834213_transparent-html-logo-png-png-download.png",
-          "title": "C# prject",
+        { 
+          "projectlanguage": "C#",
+          "imagelink": 'https://www.pngfind.com/pngs/m/683-6834213_transparent-html-logo-png-png-download.png',
+          "title": "2",
           "description":" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasratione vel inventore labore commodi modi quos culpa aut saepe!Alias!",
           "projectlink":"#"
         },
-        {
-          "imagelink": "https://www.pngfind.com/pngs/m/683-6834213_transparent-html-logo-png-png-download.png",
-          "title": "C# prject",
-          "description":" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasratione vel inventore labore commodi modi quos culpa aut saepe!Alias!",
-          "projectlink":"#"
-        }
-
-        ,
-        {
-          "imagelink": "https://www.pngfind.com/pngs/m/683-6834213_transparent-html-logo-png-png-download.png",
-          "title": "C# prject",
+        { 
+          "projectlanguage": "java",
+          "imagelink": 'https://www.pngfind.com/pngs/m/683-6834213_transparent-html-logo-png-png-download.png',
+          "title": "java",
           "description":" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasratione vel inventore labore commodi modi quos culpa aut saepe!Alias!",
           "projectlink":"#"
         }
       ]};
-      
-      let text ="";
+
+      document.getElementById("project-card").innerHTML ="";
+  if(seletedproject === "all"){
+    document.getElementById("project-card").innerHTML  ="";
     for (let i = 0 ; i < car.data.length ; i++) {
-    
-      document.getElementById("project-card").innerHTML += "<div class='column'> <div class='card'><img src='"+ car.data[i].imagelink+"' alt='greeny-earth'class='project-pic'/><h3 class='project-title'>"+ car.data[i].title+"</h3><p class='project-details'>"+ car.data[i].description+"</p><a href='"+ car.data[i].projectlink+"' target='_blank' class='project-link'> Visit Link </a></div></div>";
      
+       document.getElementById("project-card").innerHTML += "<div class='column'> <div class='card'><img src='"+ car.data[i].imagelink+"' alt='greeny-earth'class='project-pic'/><h3 class='project-title'>"+ car.data[i].title+"</h3><p class='project-details'>"+ car.data[i].description+"</p><a href='"+ car.data[i].projectlink+"' target='_blank' class='project-link'> Visit Link </a></div></div>";
     }
+  }
+  else{
+    for (let j = 0 ; j < car.data.length  ; j++) {
     
+      if(seletedproject  == car.data[j].projectlanguage){
+       document.getElementById("project-card").innerHTML += "<div class='column'> <div class='card'><img src='"+ car.data[j].imagelink+"' alt='greeny-earth'class='project-pic'/><h3 class='project-title'>"+ car.data[j].title+"</h3><p class='project-details'>"+ car.data[j].description+"</p><a href='"+ car.data[j].projectlink+"' target='_blank' class='project-link'> Visit Link </a></div></div>";
+      }
+     }
+    
+  }}
