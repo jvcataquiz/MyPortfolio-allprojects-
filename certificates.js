915db20ca93e792565a31b94509e3db64a5294a3
jvcataquiz <jvcataquiz@gmail.com>
2022-09-15 13:56:56 +0800

    seperated the certificate js to external file

--- certificates.js
+++ certificates.js
@@ -1,16 +1,27 @@
-var exampleModal = document.getElementById('exampleModal')
-exampleModal.addEventListener('show.bs.modal', function (event) {
-  // Button that triggered the modal
-  var button = event.relatedTarget
-  // Extract info from data-bs-* attributes
-  var recipient = button.getAttribute('data-bs-whatever')
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
-  var modalTitle = exampleModal.querySelector('.modal-title')
-  var modalBodyInput = exampleModal.querySelector('.modal-body input')
+$('#myModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) 
+  var recipient = button.data('whatever')
+  var modal = $(this)
 
-  modalTitle.textContent = 'New message to ' + recipient
-  modalBodyInput.value = recipient
+  const data = {
+    "Cert1" :`<iframe src="https://drive.google.com/file/d/11QZAawz8z-1B4MPPRdOWAXT_d9F2Ru-0/preview" width="100%"  class="frameheight" allow="autoplay"></iframe>`,
+    "Python1": `<iframe src="https://drive.google.com/file/d/1Y3MB_LFDkwNwTs2yGrLxUTw0-vGeJFOR/preview" width="100%"  class="frameheight" allow="autoplay"></iframe>`,
+    "Python2": `<iframe src="https://drive.google.com/file/d/1Y5c6EKR6C6m4EDGKil_w1mXj0upOoZwY/preview" width="100%"  class="frameheight" allow="autoplay"></iframe>`,
+    "Python3": `<iframe src="https://drive.google.com/file/d/1YXyohhu36uezgfZl5YHNIIYCadruEkjD/preview" width="100%"  class="frameheight" allow="autoplay"></iframe>`,
+    "Python4": `<iframe src="https://drive.google.com/file/d/1YNKAH3DSt1-zc92ckuW-NO5aJ8u63wUD/preview" width="100%"  class="frameheight" allow="autoplay"></iframe>`,
+    "Python5": `<iframe src="https://drive.google.com/file/d/1iIbEMBpvdy-iCy-zKGmzIBKBPIQHG9Cc/preview" width="100%"  class="frameheight" allow="autoplay"></iframe>`,
+    "Python6": `<iframe src="https://drive.google.com/file/d/1NdKV1K1B6ChmuMT_5PcA9i9-wWJ6pjKs/preview" width="100%"  class="frameheight" allow="autoplay"></iframe>`,
+    "Web1": `<iframe src="https://drive.google.com/file/d/1Zmcu6GHJHzPIHbhcCry-Qbs8meSZn4U_/preview" width="100%"  class="frameheight" allow="autoplay"></iframe>`,
+    "Web2": `<iframe src="https://drive.google.com/file/d/1Zb1q9MiPMagtWZqXRNZsqxvVQHZlzqRZ/preview" width="100%"  class="frameheight" allow="autoplay"></iframe>`,
+    "Web3": `<iframe src="https://drive.google.com/file/d/1Ypx1NspB9vaTuXGNEhxZ8zc4Sp7FeOpW/preview" width="100%"  class="frameheight" allow="autoplay"></iframe>`,
+    "Web4": `<iframe src="https://drive.google.com/file/d/1Z-1BBUSX3ZtcMqBOEqf_p9KnNOdiq2N3/preview" width="100%"  class="frameheight" allow="autoplay"></iframe>`,
+    "Web5": `<iframe src="https://drive.google.com/file/d/1T2Oc04MtFZjN3kpykaW94JaGxUjoYkkY/preview" width="100%"  class="frameheight" allow="autoplay"></iframe>`,
+    "Web6": `<iframe src="https://drive.google.com/file/d/1_N0tABe2P5xhhgzpskAjAMp9QFvcn6jJ/preview" width="100%"  class="frameheight" allow="autoplay"></iframe>`
+    
+  }
+
+  var datavalue = data[recipient]
+  // modal.find('.modal-title').text(recipient + ' Certificate' )
+  modal.find('.modal-body').html(datavalue)
+
 })
